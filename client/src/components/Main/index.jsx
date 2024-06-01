2f1ecc488e728ca201f7fa2c211cee0f44e0dc16
Patryk Sroczyński <patryksroczynski13@gmail.com>
2024-06-01 15:49:12 +0200

    Implement push notification

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -3,9 +3,32 @@ import FinanceForm from "../FinanceForm";
 
 const Main = () => {
 
+	const notifyMe = () => {
+		if (!("Notification" in window)) {
+			// Check if the browser supports notifications
+			alert("This browser does not support desktop notification");
+		} else if (Notification.permission === "granted") {
+			// Check whether notification permissions have already been granted;
+			// if so, create a notification
+			const notification = new Notification("Hi there!");
+			// …
+		} else if (Notification.permission !== "denied") {
+			// We need to ask the user for permission
+			Notification.requestPermission().then((permission) => {
+				// If the user accepts, let's create a notification
+				if (permission === "granted") {
+					const notification = new Notification("Hi there!");
+					// …
+				}
+			});
+		}
+	}
 
 	return (
 		<div className={styles.main}>
+			<button onClick={() => {
+				notifyMe()
+			}}>Notify me!</button>
 			<FinanceForm />
 		</div>
 	);
